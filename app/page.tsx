d3da67c582507f329aed87c49280f8dc17af3467
harold <rph1010522@est.univalle.edu>
2023-11-30 11:19:29 -0400

    fix: style

--- app/page.tsx
+++ app/page.tsx
@@ -17,32 +17,32 @@ export default function Home() {
   const handleSubmit = async (e: React.FormEvent) => {
     const trainingInformation = (e.target as HTMLFormElement).info.value;
     const TrainingInfoEncoded = encodeURIComponent(trainingInformation);
- 
+
     const url = `http://localhost:8000/datasource/?dataContent=${TrainingInfoEncoded}`;
     //Enviar al BackEnd
-    if(trainingInformation.trim()){
-      try{
+    if (trainingInformation.trim()) {
+      try {
         // Send a POST request to the backend
         const response = await fetch(url, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
         });
-   
+
         if (!response.ok) {
-            console.error('Failed to send message to backend');
-            return;
+          console.error('Failed to send message to backend');
+          return;
         }
-   
+
         const responseData = await response.json();
         console.log(responseData);
-   
+
         // Redirect to the desired page
         //router.push('/components/chat');
       }
       catch (error) {
-          console.error('Error:', error);
+        console.error('Error:', error);
       }
     }
     (e.target as HTMLFormElement).info.value = '';
@@ -59,7 +59,7 @@ export default function Home() {
         <p>
           No pierdas ventas en tu negocio por no responder mensajes al WhatsApp. De eso nos encargamos nosotros y ahorra 10x el sueldo que pagarías a una persona.
         </p>
-        <button onClick={handleOpenModal} className='bg-gradient-to-r from-blue-500 to-pink-500 bg-clip-text text-transparent mt-6 p-5 text-3xl rounded-2xl border hover:font-extrabold'>
+        <button onClick={handleOpenModal} className='bg-gradient-to-r from-blue-500 to-pink-500 bg-clip-text text-transparent mt-10 p-5 text-3xl rounded-2xl border hover:font-extrabold'>
           Prueba el Chatbot
         </button>
 
@@ -68,7 +68,7 @@ export default function Home() {
             <h3 className='text-xl mb-5'>Hola, antes de continuar te pedimos que ingreses información necesaria para entrenar a tu bot y éste pueda responder de forma precisa</h3>
             <form onSubmit={handleSubmit} action={'./dashboard/conversations'}>
               <textarea rows={10} className='w-full p-3 rounded-xl text-black text-sm' name='info' placeholder='Escribe aquí tu catálogo (Ej. si vendes productos, ingresa el nombre de tus productos con sus respectivos precios)'></textarea>
-              <button className=' w-full border rounded-2xl text-3xl p-2 hover:bg-white hover:text-gray-500 transition duration-300 ease-in-out' type='submit'>
+              <button className='bg-gray-500 w-full border rounded-2xl text-3xl p-2 hover:bg-blue-500 transition duration-300 ease-in-out' type='submit'>
                 Continuar
               </button>
             </form>
